5f9949c9dba6f97f3c3c9ab87207524ddefe0405
Ryugi Suguro <rsuguro@gmail.com>
2021-01-17 01:57:00 +0900

    able to create cocktail, but unable to get assosication working for ingredient

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -10,10 +10,10 @@ class CocktailsController < ApplicationController
   end
 
   def create
-    @cocktail = Cocktail.new(cocktail_param)
+    @cocktail = Cocktail.new(cocktail_params)
 
     if @cocktail.save
-      redirect to cocktails_path
+      redirect_to new_cocktail_dose_path(@cocktail)
     else
       render :new
     end
